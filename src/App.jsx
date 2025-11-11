b3da5a376c4e8164e0698a7ac316a32d1161e1f2
DarioConiglione <dariocng13@gmail.com>
2025-11-11 11:37:28 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+
+
+function App() {
+
+  const movies = [
+    { title: 'Inception', genre: 'Fantascienza' },
+    { title: 'Il Padrino', genre: 'Thriller' },
+    { title: 'Titanic', genre: 'Romantico' },
+    { title: 'Batman', genre: 'Azione' },
+    { title: 'Interstellar', genre: 'Fantascienza' },
+    { title: 'Pulp Fiction', genre: 'Thriller' },
+  ]
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
