e1024bde4875cd8985c5e9398c7a4346bfccaa38
DarioConiglione <dariocng13@gmail.com>
2025-11-11 23:24:13 +0100

    insert "fake-code" to a better understanding at beginning

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-
+const movies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 function App() {
 
-  const movies = [
-    { title: 'Inception', genre: 'Fantascienza' },
-    { title: 'Il Padrino', genre: 'Thriller' },
-    { title: 'Titanic', genre: 'Romantico' },
-    { title: 'Batman', genre: 'Azione' },
-    { title: 'Interstellar', genre: 'Fantascienza' },
-    { title: 'Pulp Fiction', genre: 'Thriller' },
-  ]
+  const [selectedGenre, setSelectedGenre] = useState("");
+  const [filteredMovies, setFilteredMovies] = useState(movies);
+
 
   return (
     <>
+
+      {/*
+    creare un campo select dove inserire i generi nelle option
+    utilizzare useState per gestire il genere selezionato
+    utilizzare useState per cambiare una lista in base alla selezione
+    creare una lista che mostra i film
+
+    
+    
+    */}
+
+
+
+
+
 
     </>
   )
